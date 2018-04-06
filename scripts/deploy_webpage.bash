fd69c05806de92e47eb323dc0e983f4aa1519c82
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-04-06 12:03:35 +0000

    Add a deploy script

---
+++ scripts/deploy_webpage.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+set -u
+
+tmpdir=$(mktemp -d)
+
+$(dirname $0)/generate_webpage.bash $tmpdir
+git -C $tmpdir init
+push_url=$(git remote show -n origin | grep "Push" | sed -e 's/^ *Push  *URL: //g')
+git -C $tmpdir remote add origin $push_url
+git -C $tmpdir checkout -b master
+git -C $tmpdir add $tmpdir/*
+git -C $tmpdir commit -m "$(LANG=C date)"
+git -C $tmpdir push -f origin master
+
+rm -rf $tmpdir
---
+++ scripts/generate_webpage.bash
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+set -u
+
+output=${1:-./output}
+mkdir -p $output
+
+# get git repositories
+## bootcards
+git clone --depth 1 https://github.com/bootcards/bootcards $output/bootcards
+rm -rf $output/bootcards/.git
+
+## npm-logos
+mkdir -p $output/images
+git clone --depth 1 https://github.com/npm/logos $output/images/npm-logos
+rm -rf $output/images/npm-logos/.git
+
+# process all files
+for file in $(find ./content -type f)
+do
+    echo $(basename $(dirname $file))
+    if ! echo $file | grep "src/" > /dev/null
+    then
+        # ignore src direcotry
+        path=$(realpath --relative-to=./content $file)
+        mkdir -p $output/$(dirname $path)
+        if [ "${file##*.}" = "pug" ]
+        then
+            #pug file
+            $(npm bin)/pug -P $file --out $output/$(dirname $path)
+        else
+            cp $file $output/$path
+        fi
+    fi
+done
